dbdf6855b9bef29989408431a617986c8e0d06bb
Jaylan <natarichjay@gmail.com>
2019-12-21 20:27:40 +0800

    refactor: simplified formatDate()

--- src/global/utils/utils.ts
+++ src/global/utils/utils.ts
@@ -12,51 +12,35 @@ const formatDuration = (parsedDuration: number[]): string => {
     return !parsedDuration[0] ? `${minute}:${second}`:`${hour}:${minute}:${second}`;
 };
 
-const parseDate = (date: string): number[] => {
-    const dt: Date = new Date(date);
-    return [ dt.getDate(), dt.getMonth(), dt.getFullYear(), dt.getHours(), dt.getMinutes() ];
-};
+const formatDate = (date: string, now?: string): string => {
+    var timeStampNow: number = now === undefined ? new Date().getTime()/1000 : new Date(now).getTime()/1000;  // in seconds
+    var timeStampDate: number = Math.trunc(new Date(date).getTime() / 1000);      // in seconds
+    var difference: number = timeStampNow - timeStampDate;
 
-const formatDate = (parsedDate: number[], now: Date): string => {
-    const curDate: number       = now.getDate();
-    const curMonth: number      = now.getMonth();
-    const curYear: number       = now.getFullYear();
-    const curHour: number       = now.getHours();
-    const curMinute: number     = now.getMinutes();
-    const uploadDate: number    = parsedDate[0];
-    const uploadMonth: number   = parsedDate[1];
-    const uploadYear: number    = parsedDate[2];
-    const uploadHour: number    = parsedDate[3];
-    const uploadMinute: number  = parsedDate[4];
-    
-    if (curYear > uploadYear) {
-        let diff: number = curYear - uploadYear;
-        return diff === 1 ? 'last year':`${diff} years ago`;
-    } else if (curMonth > uploadMonth) {
-        let diff: number = curMonth - uploadMonth;
-        return diff === 1 ? 'last month':`${diff} months ago`;
-    } else {
-        let diff: number = curDate - uploadDate;
-        if (diff < 7) {
-            if (diff >= 1)
-                return diff === 1 ? 'yesterday':`${diff} days ago`;
-            else {
-                if (curHour > uploadHour) {
-                    let hourDiff: number = curHour - uploadHour;
-                    return hourDiff === 1 ? '1 hour ago': `${hourDiff} hours ago`;
-                } else {
-                    let minuteDiff = curMinute - uploadMinute;
-                    return minuteDiff <= 1 ? 'just now':`${minuteDiff} minutes ago`;
-                };
-            };
-        } else
-            return Math.trunc(diff / 7) === 1 ? 'last week':`${diff / 7} weeks ago`;
-    };
+    // rough time interval
+    if (difference < 60*2)
+        return `just now`;
+    if (difference < 60*60)
+        return `${Math.trunc(difference / 60)} minutes ago`;
+    if (difference < 60*60*2)
+        return `1 hour ago`;
+    if (difference < 60*60*24) 
+        return `${Math.trunc(difference / (60*60))} hours ago`;
+    if (difference < 60*60*24*2)
+        return `yesterday`;
+    if (difference < 60*60*24*30)
+        return `${Math.trunc(difference / (60*60*24))} days ago`;
+    if (difference < 60*60*24*30*2)
+        return `last month`;
+    if (difference < 60*60*24*30*12)
+        return `${Math.trunc(difference / (60*60*24*30))} months ago`;
+    if (difference < 60*60*24*30*12*2)
+        return `last year`;
+    return `${Math.trunc(difference / (60*60*24*30*12))} years ago`;
 };
 
 export {
-    parseDate,
-    formatDuration,
     parseDuration,
+    formatDuration,
     formatDate,
 };
