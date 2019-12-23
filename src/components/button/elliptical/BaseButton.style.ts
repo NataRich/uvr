e1e24c346463f739b126bcb11005018aa083b110
Jaylan <natarichjay@gmail.com>
2019-12-23 21:44:02 +0800

    add: standard base button styles

---
+++ src/components/button/elliptical/BaseButton.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledBaseButton = styled.button`
+    width: 100px;
+    height: 30px;
+    border-radius: 50px;
+    border: 1px solid;
+    font-size: 14px;
+    outline: none;
+    cursor: pointer;
+    transition: background-color, color, transform 150ms ease-in-out;
+`;
