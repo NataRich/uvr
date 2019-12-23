7c9ae46d043acb1bc1cd8bc19526679a0a5cc645
Jaylan <natarichjay@gmail.com>
2019-12-23 21:43:42 +0800

    add: base button

---
+++ src/components/button/elliptical/BaseButton.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+import { StyledBaseButton } from './BaseButton.style';
+import { ButtonAttributes } from './interface';
+ 
+const BaseButton: React.FC<ButtonAttributes> = ({
+    backgroundColor,
+    defaultId,
+    defaultValue,
+    fontColor,
+    isLoading,
+    isSelected,
+    onClickHandler,
+}) => {
+    const [ didMouseDown, setDidMouseDown ] = useState<boolean>(false);
+    
+    const handleMouseDown   = () => setDidMouseDown(true);
+    const handleMouseUp     = () => setDidMouseDown(false);
+    return (
+    <StyledBaseButton id={defaultId === undefined ? '':defaultId}
+        className={isLoading === true ? '':''}
+        onClick={onClickHandler}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        style={{
+            color: isSelected ? backgroundColor:fontColor,
+            backgroundColor: isSelected ? fontColor:backgroundColor,
+            borderColor: isSelected ? backgroundColor:fontColor,
+            transform: didMouseDown ? "scale(0.8)":""
+        }}>
+        {defaultValue}
+    </StyledBaseButton>
+    );
+};
+
+export default BaseButton;
