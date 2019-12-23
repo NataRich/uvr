b8d63281fef3b41426e7ddf23c6e38c15dbad77c
Jaylan <natarichjay@gmail.com>
2019-12-23 22:10:20 +0800

    add: non-spaced and spaced center-box styles

--- src/global/style/Style.style.ts
+++ src/global/style/Style.style.ts
@@ -1,6 +1,6 @@
 import styled, { AnyStyledComponent } from 'styled-components';
 
-const CenterInBox: AnyStyledComponent = styled.div`
+const CenterBoxByRowNonSpaced: AnyStyledComponent = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
@@ -8,8 +8,37 @@ const CenterInBox: AnyStyledComponent = styled.div`
     align-items: center;
 `;
 
+const CenterBoxByRowSpaced: AnyStyledComponent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const CenterBoxByColNonSpaced: AnyStyledComponent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const CenterBoxByColSpaced: AnyStyledComponent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`
+
 export const GlobalStyled: Record<string, AnyStyledComponent> = {
-    CenterInBox,
+    CenterBoxByRowNonSpaced,
+    CenterBoxByRowSpaced,
+    CenterBoxByColNonSpaced,
+    CenterBoxByColSpaced,
 };
 
 export const GlobalConst: Record<string, string> = {
