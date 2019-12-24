ef1cb1d017cf5418f4ae8a0751ef6496b5221336
Jaylan <natarichjay@gmail.com>
2019-12-24 13:00:05 +0800

    refactor

--- src/components/button/elliptical/BaseButton.tsx
+++ src/components/button/elliptical/BaseButton.tsx
@@ -17,19 +17,19 @@ const BaseButton: React.FC<ButtonAttributes> = ({
     const handleMouseDown   = () => setDidMouseDown(true);
     const handleMouseUp     = () => setDidMouseDown(false);
     return (
-    <StyledBaseButton id={defaultId === undefined ? '':defaultId}
-        className={isLoading === true ? '':''}
-        onClick={onClickHandler}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        style={{
-            color: isSelected ? backgroundColor:fontColor,
-            backgroundColor: isSelected ? fontColor:backgroundColor,
-            borderColor: isSelected ? backgroundColor:fontColor,
-            transform: didMouseDown ? "scale(0.8)":""
-        }}>
-        {defaultValue}
-    </StyledBaseButton>
+        <StyledBaseButton id={defaultId === undefined ? '':defaultId}
+            className={isLoading === true ? '':''}
+            onClick={onClickHandler}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            style={{
+                color: isSelected ? backgroundColor:fontColor,
+                backgroundColor: isSelected ? fontColor:backgroundColor,
+                borderColor: isSelected ? backgroundColor:fontColor,
+                transform: didMouseDown ? "scale(0.8)":""
+            }}>
+            {defaultValue}
+        </StyledBaseButton>
     );
 };
 
