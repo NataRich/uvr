a5909f54ab8825554d5e6b630a35c9e3b89760ac
Jaylan <natarichjay@gmail.com>
2019-12-25 19:17:30 +0800

    add: loading animation

--- src/components/button/rounded/RoundedButton.style.ts
+++ src/components/button/rounded/RoundedButton.style.ts
@@ -1,15 +1,33 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 import { RoundedButtonStyle } from './interface';
 
-export const StyledRoundedButton = styled.button<RoundedButtonStyle>`
-    width: ${props => props.width}px;
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const animationMixin = css`
+    animation: ${rotate} 2s 300ms linear infinite;
+`;
+
+export const StyledRoundedButton = styled.button<RoundedButtonStyle & {isLoading: boolean}>`
+    width: ${props => props.isLoading ? props.height:props.width}px;
     height: ${props => props.height}px;
-    border-radius: ${props => props.borderRadius}px;
-    background-color: ${props => props.backgroundColor};
-    color: ${props => props.fontColor};
+    border-radius: ${props => props.isLoading ? props.height:props.borderRadius}px;
+    background-color: ${props => props.isLoading ? '#FFF':props.backgroundColor};
+    color: ${props => props.isLoading ? 'transparent':props.fontColor};
     outline: none;
-    border: none;
+    border: ${props => props.isLoading ? '5px solid':'none'};
+    border-color: ${props => props.isLoading ? props.backgroundColor: ''};
+    border-right-color: ${props => props.isLoading ? '#149E9A':''};
     font-size: 14px;
     cursor: pointer;
+    transition: width 300ms 0ms ease-in-out;
+    ${props => props.isLoading ? animationMixin:'animation: none'};
 `;
