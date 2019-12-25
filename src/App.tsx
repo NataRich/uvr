91ae410155605d579fe9e6b506b86dfe01f253f1
Jaylan <natarichjay@gmail.com>
2019-12-26 02:11:22 +0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
+    <div className="App" style={{
+      height: '100vh'
+    }}>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+      </Router>
     </div>
   );
 }
