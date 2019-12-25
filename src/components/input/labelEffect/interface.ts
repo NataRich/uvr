3d0e777b378d1d59d05e8fd71d1993bcb26f5a1d
Jaylan <natarichjay@gmail.com>
2019-12-25 18:00:37 +0800

    refactor: type declaration

--- src/components/input/labelEffect/interface.ts
+++ src/components/input/labelEffect/interface.ts
@@ -1,16 +1,20 @@
 export interface InputAttributes {
-    style: {
-        borderColor:        string;
-        borderRadius:       number;
-        fontSize:           number;
-        helperColor?:       string;
-        width:              number;
-    },
-    props: {
-        helperText?:        string;
-        isRequired:         boolean;
-        placeholder:        string;
-        value:              string;
-    },
-    onChangeHandler:    React.EventHandler<React.ChangeEvent<HTMLInputElement>>;
+    style: Style;
+    props: Props;
+    onChangeHandler: React.EventHandler<React.ChangeEvent<HTMLInputElement>>;
+};
+
+export interface Style {
+    borderColor:        string;
+    borderRadius:       number;
+    fontSize:           number;
+    helperColor?:       string;
+    width:              number;
+};
+
+export interface Props {
+    helperText?:        string;
+    isRequired:         boolean;
+    placeholder:        string;
+    value:              string;
 };
