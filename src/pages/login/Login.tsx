7e3dcac8b32ce3011ded761653610a0260ce99f5
Jaylan <natarichjay@gmail.com>
2019-12-25 19:18:39 +0800

    add: login form <Form /> container

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { GlobalStyled } from '../../global/style/Style.style';
+import Form from '../../containers/login/Form';
 
 const Login: React.FC = () => {
     return (
         <GlobalStyled.Box.CenterBoxByRowNonSpaced>
-            <GlobalStyled.Box.CenterBoxByColSpaced>
-                {/* Login form */}
-            </GlobalStyled.Box.CenterBoxByColSpaced>
+            <GlobalStyled.Box.CenterBoxByColNonSpaced>
+                <Form />
+            </GlobalStyled.Box.CenterBoxByColNonSpaced>
             <GlobalStyled.Box.CenterBoxByColNonSpaced>
                 {/* Sign Up entry + absolutely positioned logo on top right corner */}
             </GlobalStyled.Box.CenterBoxByColNonSpaced>
