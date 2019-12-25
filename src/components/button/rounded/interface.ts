e7747d1a7220ca96bca40d8c46a4e641b4a4ecd0
Jaylan <natarichjay@gmail.com>
2019-12-25 18:24:48 +0800

    update: all style properties are required

--- src/components/button/rounded/interface.ts
+++ src/components/button/rounded/interface.ts
@@ -1,19 +1,19 @@
 export interface RoundedButtonAttributes {
-    style: RoundedButtonStyle;
-    props: RoundedButtonProps;
-    onClickHandler:     React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
+    style:          RoundedButtonStyle;
+    props:          RoundedButtonProps;
+    onClickHandler: React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
 };
 
 export interface RoundedButtonStyle {
-    width?:             string;
-    height?:            string;
-    borderRadius?:      string;
+    width:              number;
+    height:             number;
+    borderRadius:       number;
     backgroundColor:    string;
     fontColor:          string;
 };
 
 export interface RoundedButtonProps {
-    defaultValue: string;
+    defaultValue:   string;
 };
 
 export interface GroupButtonAttributes {
