2b7225c7ac351f19dfbb3f0c8c4c7ed6a3d31d2c
Jaylan <natarichjay@gmail.com>
2019-12-25 19:16:13 +0800

    add: padding-bottom on container

--- src/components/input/labelEffect/Input.style.ts
+++ src/components/input/labelEffect/Input.style.ts
@@ -23,7 +23,7 @@ const StyledInputContainer = styled.div`
     width: auto;
     height: auto;
     border:none;
-    padding-top: 5%;
+    padding: 5% 0 5% 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
