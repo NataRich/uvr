8bf05d8a37cb5bcf8a7804ca8f64a710394df853
Jaylan <natarichjay@gmail.com>
2019-12-25 11:44:57 +0800

    add: input bar styles

---
+++ src/components/input/iconInFront/Input.style.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+type ContainerProps = {
+    hasBorder:      boolean;
+    borderColor:    string;
+    borderRadius:   number | undefined;
+    inputWidth:     number;
+    imgWidth:       number;
+}
+
+type ImgProps = {
+    imgWidth:       number;
+};
+
+type InputProps = {
+    borderColor:    string;
+    inputWidth:     number;
+    imgWidth:       number;
+    fontSize:       number;
+    hasBorder:      boolean;
+};
+
+const StyledInputContainer = styled.div<ContainerProps>`
+    width: ${props => props.inputWidth + props.imgWidth}px;
+    height: ${props => props.imgWidth}px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 3px;
+    border: ${props => props.hasBorder ? '1.5px solid': 'none'};
+    border-color: ${props => props.hasBorder ? props.borderColor: ''};
+    border-radius: ${props => props.hasBorder ? props.borderRadius:0}px;
+`;
+
+const StyledBox = styled.div`
+    width: auto;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const StyledImg = styled.img<ImgProps>`
+    width: ${props => props.imgWidth}px;
+    height: ${props => props.imgWidth}px;
+`;
+
+const StyledInput = styled.input<InputProps>`
+    width: ${props => props.inputWidth}px;
+    height: 90%;
+    font-size: ${props => props.fontSize}px;
+    outline: none;
+    border: none;
+    padding-bottom: 5%;
+    border-bottom: ${props => props.hasBorder ? 'none':'1px solid'};
+    border-color: ${props => props.borderColor};
+    ::placeholder {
+        font-size: ${props => props.fontSize - 1}px;
+        color: #D3D3D3;
+    }
+`;
+
+export {
+    StyledInputContainer,
+    StyledBox,
+    StyledImg,
+    StyledInput,
+};
