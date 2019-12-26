25081f6fcc93611de1a4433b58b4edb1b1305240
Jaylan <natarichjay@gmail.com>
2019-12-26 12:47:09 +0800

    add: regex

--- src/containers/login/form/Form.tsx
+++ src/containers/login/form/Form.tsx
@@ -32,17 +32,36 @@ const Form: React.FC = () => {
     const [ password, setPassword ]                     = useState<PasswordConfirmation>(defaultPassword);
 
     const onChangeAccountHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAccountAttri({...defaultAccountAttributes, props: {...defaultAccountAttributes.props, value: e.currentTarget.value}});
-        setAccount(e.currentTarget.value.includes('@') ? 
-                {...defaultAccount, account: {email: e.currentTarget.value}} :
-                {...defaultAccount, account: {username: e.currentTarget.value}});
+        const value: string = e.currentTarget.value;
+        if (value.match(/^[a-z\d]{5,12}$/)) {
+            setAccountAttri({...defaultAccountAttributes, 
+                            props: {...defaultAccountAttributes.props, value: value, helperText: 'Okay.'}});
+            setConfirmButtonAttri({...defaultConfirmButtonAttri,
+                                props: {...defaultConfirmButtonAttri.props, disabled: false}});
+            setAccount({...defaultAccount, account: {username: value}});
+        } else if (value.match(/^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})$/)) {
+            setAccountAttri({...defaultAccountAttributes, 
+                            props: {...defaultAccountAttributes.props, value: value, helperText: 'Okay.'}});
+            setConfirmButtonAttri({...defaultConfirmButtonAttri,
+                                props: {...defaultConfirmButtonAttri.props, disabled: false}});
+            setAccount({...defaultAccount, account: {email: value}});
+        } else if (value === '') {
+            setAccountAttri({...defaultAccountAttributes});
+            setConfirmButtonAttri({...defaultConfirmButtonAttri});
+            setAccount({...defaultAccount});
+        } else {
+            setAccountAttri({...defaultAccountAttributes,
+                            style: {...defaultAccountAttributes.style, borderColor: 'red', helperColor: 'red'},
+                            props: {...defaultAccountAttributes.props, value: value, helperText: 'At least 5 characters. Do not enter illegal characters.'}});
+            setConfirmButtonAttri({...defaultConfirmButtonAttri});
+            setAccount({...defaultAccount});
+        };
     };
 
     const onClickConfirmHandler = async () => {
         // Confirm if the account exists
         setConfirmButtonAttri({...confirmButtonAttri, 
                             props: {...confirmButtonAttri.props, isLoading: true}});
-        
         let status: number = (await Middleware.getStatus(API.postUsernameOrEmail(account.account))).status;
         setConfirmButtonAttri({...confirmButtonAttri, 
                             props: {...confirmButtonAttri.props, isLoading: false}});
@@ -57,15 +76,31 @@ const Form: React.FC = () => {
         else if (status === 2000) {
             setAccountAttri({...accountAttri,
                             style: {...accountAttri.style, borderColor: '#149E9A'},
-                            props: {...accountAttri.props, helperText: ''}});
+                            props: {...accountAttri.props, helperText: 'Confirmed.'}});
             setAccount({...account, isConfirmed: true})
         } else
             throw 'Unknown Error';
     };
 
     const onChangePasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPasswordAttri({...defaultPasswordAttributes, props: {...defaultPasswordAttributes.props, value: e.currentTarget.value}});
-        setPassword({...defaultPassword, password: {password: e.currentTarget.value}});
+        const value: string = e.currentTarget.value;
+        if (value.match(/^[a-z\d]{8,}$/)) {
+            setPasswordAttri({...defaultPasswordAttributes, 
+                            props: {...defaultPasswordAttributes.props, value: value, helperText: 'Okay.'}});
+            setLoginButtonAttri({...defaultLogInButtonAttri,
+                                props: {...defaultLogInButtonAttri.props, disabled: false}});
+            setPassword({...defaultPassword, password: {password: value}});
+        } else if (value === '') {
+            setPasswordAttri({...defaultPasswordAttributes});
+            setLoginButtonAttri({...defaultLogInButtonAttri});
+            setPassword({...defaultPassword});
+        } else {
+            setPasswordAttri({...defaultPasswordAttributes,
+                            style: {...defaultPasswordAttributes.style, borderColor: 'red', helperColor: 'red'},
+                            props: {...defaultPasswordAttributes.props, value: value, helperText: 'At least 8 characters. Do not enter illegal characters.'}});
+            setLoginButtonAttri({...defaultLogInButtonAttri});
+            setPassword({...defaultPassword});
+        };
     };
 
     const onClickLogInHandler = async () => {
@@ -89,7 +124,8 @@ const Form: React.FC = () => {
                             props: {...passwordAttri.props, helperText: 'The password is incorrect.'}});
         else if (status === 2000) {
             setPasswordAttri({...passwordAttri,
-                            style: {...passwordAttri.style, borderColor: '#149E9A'}});
+                            style: {...passwordAttri.style, borderColor: '#149E9A'},
+                            props: {...passwordAttri.props, helperText: 'Confirmed.'}});
             setPassword({...password, isConfirmed: true});
         } else
             throw 'Unknown Error';
