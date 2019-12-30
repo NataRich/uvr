d45f652017ccaa508d90724a07c55bb735478361
Jaylan <natarichjay@gmail.com>
2019-12-31 00:01:33 +0800

    add: 'Find' button

--- src/containers/home/search/Search.tsx
+++ src/containers/home/search/Search.tsx
@@ -1,39 +1,51 @@
 import React, { useState } from 'react';
 
 import { GlobalStyled } from '../../../global/style/Style.style';
+import { VideoGeneralAPI } from '../../../global/videos/request';
+import { VideoFilterArgInterface } from '../../../global/videos/interface';
+import { APIMiddlewares } from '../../../middlewares/API/APIMiddlewares';
 import Input from '../../../components/input/iconInFront/Input';
+import GroupButton from '../../../components/button/elliptical/Group';
+import Button from '../../../components/button/rounded/RoundedButton';
+import Filter from '../../../assets/icons/filter.svg';
 import {
+    SearchProps,
     LocalInputAttributes,
-    LocalButtonAttributes,
+    LocalEButtonAttributes,
+    LocalRButtonAttributes,
 } from './Logistics';
 import {
     defaultInputAttributes,
     defaultSortGroupButtonAttributes,
     defaultOrderGroupButtonAttributes,
+    defaultFindButtonAttributes,
 } from './Logistics';
-import Filter from '../../../assets/icons/filter.svg';
 import {
     StyledFilterIconContainer,
     StyledFilterOptionContainer,
 } from './Search.style';
-import GroupButton from '../../../components/button/elliptical/Group';
 
-const Search: React.FC = () => {
+const API           = new VideoGeneralAPI();
+const Middleware    = new APIMiddlewares();
+
+const Search: React.FC<SearchProps> = ({
+    setVideos,
+    page,
+}) => {
     const [ inputAttri, setInputAttri ]                 = useState<LocalInputAttributes>(defaultInputAttributes);
-    const [ sortGroupBtnAttri, setSortGroupBtnAttri ]   = useState<LocalButtonAttributes[]>(defaultSortGroupButtonAttributes);
-    const [ orderGroupBtnAttri, setOrderGroupBtnAttri ] = useState<LocalButtonAttributes[]>(defaultOrderGroupButtonAttributes);
+    const [ sortGroupBtnAttri, setSortGroupBtnAttri ]   = useState<LocalEButtonAttributes[]>(defaultSortGroupButtonAttributes);
+    const [ orderGroupBtnAttri, setOrderGroupBtnAttri ] = useState<LocalEButtonAttributes[]>(defaultOrderGroupButtonAttributes);
+    const [ openFilter, setOpenFilter ]                 = useState<boolean>(false);
+    const [ findBtnAttri, setFindBtnAtri ]              = useState<LocalRButtonAttributes>(defaultFindButtonAttributes);
 
-    const onChangeSearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value: string = e.currentTarget.value;
-        setInputAttri({...inputAttri, props: {...inputAttri.props, value}});
-    };
+    const onClickFilterHandler = () => setOpenFilter(!openFilter);
+    const onChangeSearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => setInputAttri({...inputAttri, props: {...inputAttri.props, value: e.currentTarget.value}});
 
     const onClickSortBtnHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         setSortGroupBtnAttri(prevProps => prevProps.map(prevProp => {
             if (prevProp.props.defaultId === e.currentTarget.id)
                 return {...prevProp, props: {...prevProp.props, isSelected: true}};
-            else
-                return {...prevProp, props: {...prevProp.props, isSelected: false}};
+            return {...prevProp, props: {...prevProp.props, isSelected: false}};
         }));
     };
 
@@ -41,27 +53,44 @@ const Search: React.FC = () => {
         setOrderGroupBtnAttri(prevProps => prevProps.map(prevProp => {
             if (prevProp.props.defaultId === e.currentTarget.id)
                 return {...prevProp, props: {...prevProp.props, isSelected: true}};
-            else
-                return {...prevProp, props: {...prevProp.props, isSelected: false}};
+            return {...prevProp, props: {...prevProp.props, isSelected: false}};
         }));
+    };
+
+    const onClickFindBtnHandler = async () => {
+        setFindBtnAtri({...findBtnAttri, props: {...findBtnAttri.props, isLoading: true}});
+        const order: string     = orderGroupBtnAttri.filter(object => object.props.isSelected === true).map(object => object.props.defaultValue)[0];
+        const sort_by: string   = sortGroupBtnAttri.filter(object => object.props.isSelected === true).map(object => object.props.defaultValue)[0];
+        const title: string     = inputAttri.props.value;
+        const tags: string[]    = [];       // currently unusable hence empty
+        const payload: VideoFilterArgInterface = { order, page, sort_by, tags, title };
+        setVideos((await Middleware.getVideos(API.postFilterArgs(payload))).videos);
+        setFindBtnAtri({...findBtnAttri, props: {...findBtnAttri.props, isLoading: false}});
     };
 
     return (
         <GlobalStyled.Box.CenterBoxByRowNonSpaced style={{ height: 'auto' }}>
-            <StyledFilterIconContainer {...{imgWidth: inputAttri.style.imgWidth}}>
+            <StyledFilterIconContainer {...{imgWidth: inputAttri.style.imgWidth}}
+                onClick={onClickFilterHandler}>
                 <img src={Filter} alt='' />
             </StyledFilterIconContainer>
-            <StyledFilterOptionContainer>
+            &nbsp;
+            <StyledFilterOptionContainer style={{ visibility: openFilter ? 'visible':'hidden' }}>
                 <GroupButton {...{category: 'Sort',
                                 attributes: sortGroupBtnAttri,
                                 onClickHandler: onClickSortBtnHandler}} />
+                <hr style={{ margin: '3px 0 3px 0', width: '95%' }} />
                 <GroupButton {...{category: 'Order',
                                 attributes: orderGroupBtnAttri,
-                                onClickHandler: onClickOrderBtnHandler}}/>
+                                onClickHandler: onClickOrderBtnHandler}} />
             </StyledFilterOptionContainer>
             <Input {...{style: inputAttri.style,
                         props: inputAttri.props,
                         onChangeHandler: onChangeSearchHandler}} />
+            &nbsp;
+            <Button {...{style: findBtnAttri.style,
+                        props: findBtnAttri.props,
+                        onClickHandler: onClickFindBtnHandler}}/>
         </GlobalStyled.Box.CenterBoxByRowNonSpaced>
     );
 };
