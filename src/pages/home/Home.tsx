fa82bad7088a1a74dfc69c589b8378957b13e9e1
Jaylan <natarichjay@gmail.com>
2019-12-31 19:08:22 +0800

    add: plaza component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { StyledTallContainer } from './Home.style';
 import { GlobalStyled } from '../../global/style/Style.style';
+import { VideoClassType } from '../../global/videos/class';
+import { VideoGeneralAPI } from '../../global/videos/request';
+import { VideoFilterArgInterface } from '../../global/videos/interface';
+import { APIMiddlewares } from '../../middlewares/API/APIMiddlewares';
 import Navigation from '../../containers/home/navigation/Navigation';
 import Search from '../../containers/home/search/Search';
+import Plaza from '../../containers/home/plaza/Plaza';
+
+const API           = new VideoGeneralAPI();
+const Middleware    = new APIMiddlewares();
 
 const Home: React.FC = () => {
-    const [ isAboutClicked, setIsAboutClicked ] = useState<boolean>(false);
+    const [ isAboutClicked, setIsAboutClicked ]     = useState<boolean>(false);
+    const [ isFetchingVideos, setIsFetchingVideos ] = useState<boolean>(true);
+    const [ videos, setVideos ]                     = useState<VideoClassType[] | null>(null);
+    const [ page, setPage ]                         = useState<number>(1);
+    
 
     const onClickAbout = () => setIsAboutClicked(prev => !prev);
+
+    const fetchVideos = async () => {
+        setIsFetchingVideos(true);
+        const payload: VideoFilterArgInterface = {
+            order: 'ASC',
+            page: 1,
+            sort_by: 'Date',
+            tags: [],
+            title: ''
+        };
+        setVideos((await Middleware.getVideos(API.postFilterArgs(payload))).videos);
+        setIsFetchingVideos(false);
+    }; 
+
+    useEffect(() => { 
+        // fetchVideos();
+    }, [])
 
     return (
         <StyledTallContainer>
@@ -28,15 +57,19 @@ const Home: React.FC = () => {
                 style={{ visibility: isAboutClicked ? 'visible': 'hidden' }}>
                 {/* about card to introduce the team */}
             </div>
-            <div className='p-home-search'>
-                <Search />
-            </div>
-            <div className='p-home-result'>
-                {/* video plaza */}
-            </div>
-            <div className='p-home-footer'>
+            <GlobalStyled.Box.CenterEndBoxByCol style={{ height: '15%' }}>
+                <Search {...{page, setVideos}} />
+            </GlobalStyled.Box.CenterEndBoxByCol>
+            <GlobalStyled.Box.CenterBoxByColNonSpaced style={{ height: '80%', overflowY: 'scroll' }}>
+                <Plaza {...{isFetchingVideos, page, setPage, videos}} />
+            </GlobalStyled.Box.CenterBoxByColNonSpaced>
+            <GlobalStyled.Box.CenterBoxByRowNonSpaced style={{
+                backgroundColor: 'transparent',
+                color: '#000',
+                height: '5%',
+            }}>
                 <p>®2019 UWC (CSC) Virtual Reality</p>
-            </div>
+            </GlobalStyled.Box.CenterBoxByRowNonSpaced>
         </StyledTallContainer>
     );
 };
