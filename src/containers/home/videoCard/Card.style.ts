9e8e993269b8a5bea06ec0ae3b4ab7d4b11cdb86
Jaylan <natarichjay@gmail.com>
2019-12-31 20:11:16 +0800

    update: styles

--- src/containers/home/videoCard/Card.style.ts
+++ src/containers/home/videoCard/Card.style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledVideoCardContainer = styled.div`
     width: 300px;
-    height: 256px;
+    height: 206px;
     overflow: hidden;
     box-shadow: 0 5px 15px 0 #D3D3D3;
     border-radius: 10px;
@@ -21,11 +21,22 @@ export const StyledVideoCardContainer = styled.div`
             align-items: center;
             .con-home-plaza-vcard-title {
                 width: 95%;
-                height: auto;
+                height: 17px;
                 p { 
+                    font-size: 14px;
                     font-family: 'Lato', sans-serif;
-                    justify-content: flex-start;
                     font-weight: bold;
+                    justify-content: center;
+                };
+            };
+            .overflow {
+                overflow-x: scroll;
+                ::-webkit-scrollbar {
+                    display: none;
+                };
+                p {
+                    width: 150%;
+                    justify-content: flex-start;
                 };
             };
             .con-home-plaza-vcard-vimg {
@@ -34,18 +45,31 @@ export const StyledVideoCardContainer = styled.div`
             };
             .con-home-plaza-vcard-btn-group {
                 width: 100%;
-                height: 10%;
+                height: 30px;
                 display: flex;
                 justify-content: space-around;
                 align-items: center;
             };
             .con-home-plaza-vcard-description {
                 width: 95%;
-                height: auto;
+                height: 130px;
+                overflow-y: scroll;
+                p {
+                    font-size: 14px;
+                    color: #999;
+                    justify-content: flex-start;
+                };
             };
             .con-home-plaza-vcard-others {
+                width: 100%;
+                height: 18px;
                 float: left;
                 margin-left: 5px;
+                P {
+                    color: #B3B3B3;
+                    font-size: 14px;
+                    justify-content: flex-start;
+                }
             }
         };
     };
--- src/containers/home/videoCard/Card.tsx
+++ src/containers/home/videoCard/Card.tsx
@@ -21,6 +21,12 @@ const VideoCard: React.FC<VideoCardAttributes> = ({
     const onClickMoreHandler = () => setOpenDescription(true);
     const onClickBackHandler = () => setOpenDescription(false);
 
+    const cutString = (originalStr: string, limit: number): string => {
+        if (originalStr.length > limit)
+            return `${originalStr.substring(0, limit)}...`;
+        return originalStr;
+    };
+
     return (
         <StyledVideoCardContainer>
             <div className='track'
@@ -33,7 +39,7 @@ const VideoCard: React.FC<VideoCardAttributes> = ({
                     </div>
                     <div className='con-home-plaza-vcard-title'>
                         <GlobalStyled.Text.FullWidthAutoHeightNonMargin>
-                            {video.getTitle()}
+                            {cutString(video.getTitle(), 38)}       {/* 38 is the maximum number of characters that it can contain inline */}
                         </GlobalStyled.Text.FullWidthAutoHeightNonMargin>
                     </div>
                     <div className='con-home-plaza-vcard-btn-group'>
@@ -46,19 +52,19 @@ const VideoCard: React.FC<VideoCardAttributes> = ({
                     </div>
                 </div>
                 <div className='slide'>
-                    <div className='con-home-plaza-vcard-title'>
-                        <GlobalStyled.Text.FullWidthAutoHeightNonMargin>
+                    <div className={video.getTitle().length > 38 ? 'con-home-plaza-vcard-title overflow':'con-home-plaza-vcard-title'}>
+                        <GlobalStyled.Text.AutoWidthFullHeightNonMargin>
                             {video.getTitle()}
-                        </GlobalStyled.Text.FullWidthAutoHeightNonMargin>
+                        </GlobalStyled.Text.AutoWidthFullHeightNonMargin>
                     </div>
                     <div className='con-home-plaza-vcard-description'>
-                        <GlobalStyled.Text.FullWidthAutoHeightNonMargin style={{ justifyContent: 'flex-start' }}>
+                        <GlobalStyled.Text.FullWidthAutoHeightNonMargin>
                             {video.getDescription()}
                         </GlobalStyled.Text.FullWidthAutoHeightNonMargin>
                     </div>
                     <div className='con-home-plaza-vcard-others'>
-                        <GlobalStyled.Text.FullWidthAutoHeightNonMargin style={{ width: 'auto', color: '#B3B3B3' }}>
-                            {video.getAuthor()}&nbsp;&#8226;&nbsp;{video.getUploadTime()}
+                        <GlobalStyled.Text.FullWidthAutoHeightNonMargin>
+                            <span style={{color: '#1C3144'}}>{video.getAuthor()}</span>&nbsp;&#8226;&nbsp;{video.getUploadTime()}
                         </GlobalStyled.Text.FullWidthAutoHeightNonMargin>
                     </div>
                     <div className='con-home-plaza-vcard-btn-group'>
