934f25103dae022f4f65ac209fbf4c69bd012a25
Jaylan <natarichjay@gmail.com>
2020-01-01 14:13:54 +0800

    add: account container

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 import { GlobalStyled } from '../../global/style/Style.style';
+import Account from '../../containers/profile/account/Account';
+import { UserClassType } from '../../global/user/class';
 
-const Profile: React.FC = () => {
+type ProfileProps = {
+    user: UserClassType;
+};
+
+const Profile: React.FC<ProfileProps> = ({
+    user,
+}) => {
     return (
         <GlobalStyled.Box.CenterBoxByRowNonSpaced>
-            <GlobalStyled.Box.CenterBoxByColNonSpaced>
+            <GlobalStyled.Box.CenterBoxByColNonSpaced style={{
+                width: '315px',
+                boxShadow: '5px 0 20px 10px #EEE',
+                }}>
                 {/* personal information */}
+                <Account {...{user}} />
             </GlobalStyled.Box.CenterBoxByColNonSpaced>
-            <GlobalStyled.Box.CenterBoxByColNonSpaced>
+            <GlobalStyled.Box.CenterBoxByColNonSpaced style={{ width: 'calc(100% - 315px)' }}>
                 {/* video & upload video action space */}
             </GlobalStyled.Box.CenterBoxByColNonSpaced>
         </GlobalStyled.Box.CenterBoxByRowNonSpaced>
