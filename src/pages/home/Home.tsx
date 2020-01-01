d52d9d4826441ba7bbc996bd52dbf2aa53646c44
Jaylan <natarichjay@gmail.com>
2020-01-01 15:09:59 +0800

    refactor: async API calls

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import { StyledTallContainer } from './Home.style';
 import { GlobalStyled } from '../../global/style/Style.style';
+import { UserClassType } from '../../global/user/class';
+import { UserGeneralAPI } from '../../global/user/request';
 import { VideoClassType } from '../../global/videos/class';
 import { VideoGeneralAPI } from '../../global/videos/request';
 import { VideoFilterArgInterface } from '../../global/videos/interface';
@@ -10,36 +12,53 @@ import Navigation from '../../containers/home/navigation/Navigation';
 import Search from '../../containers/home/search/Search';
 import Plaza from '../../containers/home/plaza/Plaza';
 
-const API           = new VideoGeneralAPI();
+const UAPI          = new UserGeneralAPI();
+const VAPI          = new VideoGeneralAPI();
 const Middleware    = new APIMiddlewares();
 
 const Home: React.FC = () => {
     const [ isAboutClicked, setIsAboutClicked ]     = useState<boolean>(false);
+    const [ isFetchingUser, setIsFetchingUser ]     = useState<boolean>(true);
     const [ isFetchingVideos, setIsFetchingVideos ] = useState<boolean>(true);
     const [ videos, setVideos ]                     = useState<VideoClassType[] | null>(null);
+    const [ user, setUser ]                         = useState<UserClassType | null>(null);
     const [ page, setPage ]                         = useState<number>(1);
     
     const onClickAbout = () => setIsAboutClicked(prev => !prev);
 
-    const fetchVideos = async () => {
-        setIsFetchingVideos(true);
-        const payload: VideoFilterArgInterface = {
-            order: true,
-            page: 1,
-            sort_by: 'Date',
-            tags: [],
-            title: ''
+    useEffect(() => {
+        const userAbortController: AbortController = new AbortController();
+        const videoAbortController: AbortController = new AbortController();
+        const fetchUser = async () => {
+            setIsFetchingUser(true);
+            setUser(await Middleware.getUser(UAPI.getUser(userAbortController.signal)));
+            setIsFetchingUser(false);
         };
-        setVideos((await Middleware.getVideos(API.postFilterArgs(payload))).videos);
-        setIsFetchingVideos(false);
-    }; 
+        const fetchVideos = async () => {
+            setIsFetchingVideos(true);
+            const payload: VideoFilterArgInterface = {
+                order: true,
+                page: 1,
+                sort_by: 'Date',
+                tags: [],
+                title: ''
+            };
+            setVideos((await Middleware.getVideos(VAPI.postFilterArgs(payload, videoAbortController.signal))).videos);
+            setIsFetchingVideos(false);
+        };
 
-    useEffect(() => { fetchVideos() }, []);
-
+        fetchUser();
+        fetchVideos();
+        return () => {
+            userAbortController.abort();
+            videoAbortController.abort();
+        };
+    }, []);
+    
     return (
         <StyledTallContainer>
             <GlobalStyled.Box.CenterBoxByRowXSpaced style={{ width: '95%', height: 'auto', position: 'absolute', top: '0' }}>
-                <Navigation />
+                <Navigation {...{user, isFetchingUser}} />
             </GlobalStyled.Box.CenterBoxByRowXSpaced>
             <div className='p-home-about-btn_fixed'>
                 <button onClick={onClickAbout}
