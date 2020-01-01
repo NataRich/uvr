b1d45c571df1a9c4d605ced4f0d30daa55c9b326
Jaylan <natarichjay@gmail.com>
2020-01-01 15:53:53 +0800

    add: EUnLogIn.tsx error page

---
+++ src/pages/profile/error/EUnLogIn.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { GlobalStyled } from '../../../global/style/Style.style';
+import { LogoAttributes } from '../../../components/UVRLogo/interface';
+import Logo from '../../../components/UVRLogo/Logo';
+
+const EUnLogIn: React.FC = () => {
+    const onClickLogInHandler = () => window.location.href='/login';
+    
+    return (
+        <GlobalStyled.Box.CenterBoxByColXSpaced>
+            <div
+                style={{
+                    position: 'absolute',
+                    left: '0',
+                    top: '0',
+                }}>
+                <Logo {...defaultLogoAttributes} />
+            </div>
+            <div style={{
+                    width: '100%',
+                    height: '25%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#149E9A',
+                    color: '#FFF',
+                }}>
+                <p style={{fontSize: '30px'}}>Error 405</p>
+            </div>
+            <div>
+                <p>We have detected that you have not logged in.</p>
+                <p>If you want to access this page, please <span onClick={onClickLogInHandler} style={{ textDecoration: 'underline', color: '#149E9A', cursor: 'pointer' }}>log in</span> first</p>
+            </div>
+            <div style={{
+                    width: '100%',
+                    height: '25%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: '#149E9A',
+                    color: '#FFF',
+                }}>
+                <p>®2019 UWC (CSC) Virtual Reality</p>
+            </div>
+        </GlobalStyled.Box.CenterBoxByColXSpaced>
+    );
+};
+
+export default EUnLogIn;
+
+const defaultLogoAttributes: LogoAttributes = {
+    style: {
+        position: 'LHS',
+    },
+    props: {},
+};
