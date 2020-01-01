7caac203b7a98139bb528ab12325f68a7e0099d0
Jaylan <natarichjay@gmail.com>
2020-01-01 21:35:20 +0800

    update: height 150% to 100%

--- src/pages/home/Home.style.ts
+++ src/pages/home/Home.style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledTallContainer = styled.div`
     width: 100%;
-    height: 150%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
