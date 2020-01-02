68a85bdb3611bf35ed30bb55b1dd60fd488b0237
Jaylan <natarichjay@gmail.com>
2020-01-02 15:02:07 +0800

    add: upload video step 1

---
+++ src/containers/profile/actionSpace/steps/step1/Logistics.ts
@@ -0,0 +1,78 @@
+import {
+    RoundedButtonProps,
+    RoundedButtonStyle,
+} from '../../../../../components/button/rounded/interface';
+import {
+    LabelEffectInputProps,
+    LabelEffectInputStyle,
+} from '../../../../../components/input/labelEffect/interface';
+import { VideoLoginRequiredAPI } from '../../../../../global/videos/request';
+import { APIMiddlewares } from '../../../../../middlewares/API/APIMiddlewares';
+
+export const VAPI       = new VideoLoginRequiredAPI();
+export const Middleware = new APIMiddlewares();
+
+export type LocalRButtonAttributes = {
+    style: RoundedButtonStyle;
+    props: RoundedButtonProps;
+};
+
+export type LocalInputAttributes = {
+    style: LabelEffectInputStyle;
+    props: LabelEffectInputProps;
+};
+
+const generateTrackId = (): string => {
+    const LENGTH: number = 24;
+    let str: string = '';
+    let arr: any[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 
+                    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+                    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    for (let i = 0; i < LENGTH; i++) {
+        let p: number = Math.round(Math.random() * (arr.length - 1));
+        str += arr[p];
+    }
+    return str;
+};
+
+const defaultTrackIdInputAttributes: LocalInputAttributes = {
+    style: {
+        width: 200,
+        borderColor: '#1C3144',
+        borderRadius: 5,
+        fontSize: 14,
+    },
+    props: {
+        value: '',
+        isRequired: true,
+        placeholder: '',
+        type: 'text',
+    },
+};
+
+const defaultAcquireTrackIdBtnAttributes: LocalRButtonAttributes = {
+    style: {
+        width: 150,
+        height: 40,
+        borderColor: 'transparent',
+        borderRadius: 5,
+        borderWidth: 5,
+        backgroundColor: '#1C3144',
+        fontColor: '#FFF',
+        fontSize: 16,
+        invertColorH: false,
+        loadingBorderColor: '#18626B'
+    },
+    props: {
+        defaultValue: 'Acquire Track ID',
+        disabled: false,
+        isLoading: false,
+    },
+};
+
+export {
+    defaultTrackIdInputAttributes,
+    defaultAcquireTrackIdBtnAttributes,
+    generateTrackId,
+};
---
+++ src/containers/profile/actionSpace/steps/step1/Step1.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import { TrackIdInterface } from '../../../../../global/videos/interface';
+import Button from '../../../../../components/button/rounded/RoundedButton';
+import Input from '../../../../../components/input/labelEffect/Input';
+import {
+    LocalRButtonAttributes,
+    LocalInputAttributes,
+} from './Logistics';
+import {
+    defaultTrackIdInputAttributes,
+    defaultAcquireTrackIdBtnAttributes,
+    generateTrackId,
+} from './Logistics';
+import { VAPI, Middleware } from './Logistics';
+
+
+
+const Step1: React.FC = () => {
+    const [ acquireBtnAttri, setAcquireBtnAttri ]   = useState<LocalRButtonAttributes>(defaultAcquireTrackIdBtnAttributes);
+    const [ trackIdAttri, setTrackIdAttri ]         = useState<LocalInputAttributes>(defaultTrackIdInputAttributes);
+
+    const onChangeHandler = () => {};
+
+    const onClickAcquireHandler = async () => {
+        setAcquireBtnAttri({...acquireBtnAttri, props: {...acquireBtnAttri.props, isLoading: true}});
+        const trackIdAbortController: AbortController = new AbortController();
+        let temp: string = generateTrackId();
+        let payload: TrackIdInterface = { track_id: temp};
+        let status: number = (await Middleware.getStatus(VAPI.postTrackId(payload, trackIdAbortController.signal))).status;
+        while (status !== 2000) {
+            temp = generateTrackId();
+            payload = { track_id: temp };
+            status = (await Middleware.getStatus(VAPI.postTrackId(payload, trackIdAbortController.signal))).status;
+        };
+        setTrackIdAttri({...trackIdAttri, props: {...trackIdAttri.props, value: temp}});
+        trackIdAbortController.abort();
+        setAcquireBtnAttri({...acquireBtnAttri, props: {...acquireBtnAttri.props, isLoading: false}});
+    };
+
+    return(
+        <>
+            <Input {...{style: trackIdAttri.style,
+                        props: trackIdAttri.props,
+                        onChangeHandler}} />
+            <Button {...{style: acquireBtnAttri.style,
+                        props: acquireBtnAttri.props,
+                        onClickHandler: onClickAcquireHandler}} />
+        </>
+    );
+};
+
+export default Step1;
