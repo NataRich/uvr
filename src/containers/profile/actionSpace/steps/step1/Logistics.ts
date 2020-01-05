5ae9f9585c12eb928969a9a71cf2ff0450b7b5e0
Jaylan <natarichjay@gmail.com>
2020-01-05 14:09:10 +0800

    close: issue #13

--- src/containers/profile/actionSpace/steps/step1/Logistics.ts
+++ src/containers/profile/actionSpace/steps/step1/Logistics.ts
@@ -17,20 +17,6 @@ export type LocalInputAttributes = {
     props: LabelEffectInputProps;
 };
 
-const generateTrackId = (): string => {
-    const LENGTH: number = 24;
-    let str: string = '';
-    let arr: any[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-                    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 
-                    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-                    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    for (let i = 0; i < LENGTH; i++) {
-        let p: number = Math.round(Math.random() * (arr.length - 1));
-        str += arr[p];
-    }
-    return str;
-};
-
 const defaultTrackIdInputAttributes: LocalInputAttributes = {
     style: {
         width: 200,
@@ -69,5 +55,4 @@ const defaultAcquireTrackIdBtnAttributes: LocalRButtonAttributes = {
 export {
     defaultTrackIdInputAttributes,
     defaultAcquireTrackIdBtnAttributes,
-    generateTrackId,
 };
