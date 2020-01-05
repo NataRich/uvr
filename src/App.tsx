0c1b654ff01d711ad29f1b2280d966eb50bb0523
Jaylan <natarichjay@gmail.com>
2020-01-05 15:14:05 +0800

    add: 404 error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Error404 from './pages/404/404';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Home from './pages/home/Home';
@@ -10,10 +11,11 @@ const App: React.FC = () => {
   return (
     <div style={{ height: '100vh' }}>
       <Router>
-        <Route exact path='/' component={() => <Home />} />
-        <Route path='/login' component={() => <Login />} />
-        <Route path='/signup' component={() => <Signup />} />
-        <Route path='/profile' component={() => <Profile />} />
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/profile' component={Profile} />
+        <Route path='#' component={Error404} />
       </Router>
     </div>
   );
---
+++ src/pages/404/404.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { GlobalStyled } from '../../global/style/Style.style';
+import Logo from '../../components/UVRLogo/Logo';
+import { LogoAttributes } from '../../components/UVRLogo/interface';
+
+const Error404: React.FC = () => {
+    return (
+        <GlobalStyled.Box.CenterBoxByColXSpaced>
+            <div style={{
+                position: 'absolute',
+                top: '0',
+                left: '5px'
+            }}>
+                <Logo {...{style: defaultLogoAttributes.style,
+                        props: defaultLogoAttributes.props,}} />
+            </div>
+            <GlobalStyled.Box.CenterBoxByRowNonSpaced style={{ backgroundColor: '#149E9A' }}>
+                    <GlobalStyled.Text.AutoWidthAutoHeightNonMargin>
+                        404 Page
+                    </GlobalStyled.Text.AutoWidthAutoHeightNonMargin>
+            </GlobalStyled.Box.CenterBoxByRowNonSpaced>
+            <GlobalStyled.Box.CenterBoxByRowNonSpaced style={{ backgroundColor: '#FFF' }}>
+            </GlobalStyled.Box.CenterBoxByRowNonSpaced>
+            <GlobalStyled.Box.CenterBoxByRowNonSpaced style={{ backgroundColor: '#149E9A' }}>
+                <GlobalStyled.Text.AutoWidthAutoHeightNonMargin>
+                    ®2019 UWC (CSC) Virtual Reality
+                </GlobalStyled.Text.AutoWidthAutoHeightNonMargin>
+            </GlobalStyled.Box.CenterBoxByRowNonSpaced>
+        </GlobalStyled.Box.CenterBoxByColXSpaced>
+    );
+};
+
+export default Error404;
+
+const defaultLogoAttributes: LogoAttributes = {
+    style: {
+        position: 'LHS',
+    },
+    props: {},
+};
